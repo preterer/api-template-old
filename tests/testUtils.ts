bb7e05656ba58fd1d4e21e8a21b32fcc56f69ddc
Michal <preterer@gmail.com>
2019-08-22 22:11:04 +0200

    transactional conext in tests

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,5 +1,9 @@
 import * as TypeORM from "typeorm";
 import { Container } from "typedi";
+import {
+  initializeTransactionalContext,
+  patchTypeORMRepositoryWithBaseRepository
+} from "typeorm-transactional-cls-hooked";
 
 import { Fundraiser } from "../src/entities/fundraiser.entity";
 
@@ -10,6 +14,8 @@ import { Fundraiser } from "../src/entities/fundraiser.entity";
  * @returns
  */
 export function mockDB(): Promise<TypeORM.Connection> {
+  initializeTransactionalContext();
+  patchTypeORMRepositoryWithBaseRepository();
   TypeORM.useContainer(Container);
   return TypeORM.createConnection({
     type: "sqljs",
